734da45940abb6687d5d4067e1a7d1afdd6baa9d
wagasode <47851510+wagasode@users.noreply.github.com>
2022-11-30 17:34:33 +0900

    Clean

--- hxp-new_project/settings.py
+++ hxp-new_project/settings.py
@@ -1,6 +1,9 @@
 from os import environ
 SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
-SESSION_CONFIGS = [dict(name='my_survey', num_demo_participants=None, app_sequence=['yamada_test_survey']), dict(name='test_yamada_public_goods', num_demo_participants=None, app_sequence=['test_yamada_public_goods']), dict(name='test_yamada_trust_game', num_demo_participants=None, app_sequence=['test_yamada_trust_game']), dict(name='test_yamada_prisoners_dilemma_game', num_demo_participants=None, app_sequence=['test_yamada_prisoners_dilemma_game'])]
+SESSION_CONFIGS = [dict(name='my_survey', num_demo_participants=None, app_sequence=['yamada_test_survey']),
+        dict(name='test_yamada_public_goods', num_demo_participants=None, app_sequence=['test_yamada_public_goods']),
+        dict(name='test_yamada_trust_game', num_demo_participants=None, app_sequence=['test_yamada_trust_game']),
+        dict(name='test_yamada_prisoners_dilemma_game', num_demo_participants=None, app_sequence=['test_yamada_prisoners_dilemma_game'])]
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
